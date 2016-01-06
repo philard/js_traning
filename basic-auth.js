3999b2f807329ee7bdaf00866ab9fbea0e5d3a9e
Philip Ardley <philard@gmail.com>
2016-01-06 17:58:14 +0800

    sdfdsfsdf

---
+++ basic-auth.js
@@ -0,0 +1,3 @@
+/**
+ * Created by Philip on 06/01/2016.
+ */
---
+++ c.js
@@ -0,0 +1,50 @@
+'use strict';
+
+let http = require('http');
+let express = require('express');
+let app = express();
+let bodyParser = require('body-parser');
+
+let server = http.createServer(app);
+
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json());
+
+
+
+const user = 'admin';
+const pass = 'pass';
+
+app.use((req, res, next) => {
+	if (isAuthorised(req, res)) {
+		next();
+	} else {
+		res.set('WWW-Authenticate', 'Basic realm="You shall not pass!!!"');
+		res.status(401).send('Not allowd');
+	}
+});
+
+
+function isAuthorised(req, res) {
+	let header = req.headers.authorization;
+	if(!header) header = 'none none ';
+
+	let hash = header.split(' ')[1];
+	let test = new Buffer(hash, 'base64').toString('utf-8');
+
+	return test == user + ':' + pass;
+}
+
+
+
+
+
+app.use(express.static(__dirname + '/public'));
+
+
+
+
+server.listen(8080, () => {
+	console.log('hello 8080')
+});
