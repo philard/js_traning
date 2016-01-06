ce489d82127279612e053b9d29f6616b15ef9846
Philip Ardley <philard@gmail.com>
2016-01-06 20:41:04 +0800

    modularized

--- basic.js
+++ basic.js
@@ -4,12 +4,13 @@ let http = require('http');
 let express = require('express');
 let app = express();
 let bodyParser = require('body-parser');
-let auth = require('./lib/basic-auth');
+let security = require('./lib/basic-auth');
+let authorizer = require('./lib/random-auth');
 
 let server = http.createServer(app);
 
 
-app.use(auth('admin', 'pass'));
+app.use(security(authorizer));
 app.use(express.static(__dirname + '/public'));
 
 server.listen(8080, () => {
