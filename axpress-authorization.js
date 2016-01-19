17d7d1289ef0213432929351d38d0a99937f4239
Philip Ardley <philard@gmail.com>
2016-01-19 20:51:45 +0800

    Small changes and prep.

--- axpress-authorization.js
+++ axpress-authorization.js
@@ -4,14 +4,16 @@ let http = require('http');
 let express = require('express');
 let app = express();
 let bodyParser = require('body-parser');
-let auth = require('./lib/basic-auth');
+let security = require('./lib/security');
+let randomAuth = require('./lib/random-auth');
+let userPassAuth = require('./lib/user-pass-auth.js');
 
 let server = http.createServer(app);
 
 
-app.use(auth('admin', 'pass'));
+app.use(security(userPassAuth));
 app.use(express.static(__dirname + '/public'));
 
 server.listen(8080, () => {
-	console.log('hello 8080')
+    console.log('hello 8080')
 });
