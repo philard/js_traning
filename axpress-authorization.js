af6bc9c1bc9da1ee0039100469f75439692431d5
Philip Ardley <philard@gmail.com>
2016-01-19 21:58:09 +0800

    prep

--- axpress-authorization.js
+++ axpress-authorization.js
@@ -1,19 +1,24 @@
 'use strict';
 
-let http = require('http');
 let express = require('express');
+let http = require('http');
+let authorization = require('express-authorization');
+
+
 let app = express();
-let bodyParser = require('body-parser');
+
+
 let security = require('./lib/security');
-let randomAuth = require('./lib/random-auth');
 let userPassAuth = require('./lib/user-pass-auth.js');
-
-let server = http.createServer(app);
-
-
 app.use(security(userPassAuth));
+
+
+
 app.use(express.static(__dirname + '/public'));
 
+
+
+let server = http.createServer(app);
 server.listen(8080, () => {
     console.log('hello 8080')
 });
--- basic.js
+++ basic.js
@@ -4,13 +4,14 @@ let http = require('http');
 let express = require('express');
 let app = express();
 let bodyParser = require('body-parser');
-let security = require('./lib/basic-auth');
-let authorizer = require('./lib/random-auth');
+let security = require('./lib/security');
+let randomAuth = require('./lib/random-auth');
+let userPassAuth = require('./lib/user-pass-auth.js');
 
 let server = http.createServer(app);
 
 
-app.use(security(authorizer));
+app.use(security(userPassAuth));
 app.use(express.static(__dirname + '/public'));
 
 server.listen(8080, () => {
