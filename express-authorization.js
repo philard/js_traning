05d090c53cc581215d8d0f8bcaa14bf5b4be356c
Philip Ardley <philard@gmail.com>
2016-01-20 22:40:33 +0800

    Technically possible to set your own permissions to "secure:view".

--- express-authorization.js
+++ express-authorization.js
@@ -4,6 +4,7 @@ let express = require('express');
 let http = require('http');
 let cookieParser = require('cookie-parser');
 let session = require('express-session');
+let bodyParser = require('body-parser');
 let authorization = require('express-authorization');
 
 let app = express();
@@ -11,20 +12,26 @@ let app = express();
 // all environments
 app.set('view engine', 'ejs');
 //app.use(express.logger('dev'));
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/json
+app.use(bodyParser.json());
 app.use(cookieParser('your secret here'));
 app.use(session());
 
 // setup permission middleware
 authorization.ensureRequest.options = {
     onDenied:function (req, res) {
-        //See http://stackoverflow.com/questions/14573654/spring-security-redirect-to-previous-page-after-succesful-login#answer-23796392
-        req.session.urlPriorLogin = req.url;
+        req.session.urlPriorLogin = req.url;//Similar to Java http://stackoverflow.com/questions/14573654/spring-security-redirect-to-previous-page-after-succesful-login#answer-23796392
         res.redirect('/login');
-    },
+    }
     //redirectTo:'/login'
 };
-var ensureAssertView = authorization.ensureRequest.isPermitted('assert:view');
 
+var ensureAssetView = authorization.ensureRequest.isPermitted('asset:view');
+var ensureAssetEdit = authorization.ensureRequest.isPermitted('asset:edit');
+var ensureSecureView = authorization.ensureSecureView.isPermitted('secure:view');
 
 app.get('/login', function (req, res) {
     res.render('login', { });
@@ -33,16 +40,31 @@ app.get('/login', function (req, res) {
 
 app.post('/login', function (req, res) {
     req.session.user = {
-        username: "root",
-        permissions: [ 'assert:view,edit' ]
+        username: req.body.username,
+        permissions: [ 'asset:view,edit', 'secure:' ]
     };
     res.redirect(req.session.urlPriorLogin);
     delete req.session.urlPriorLogin;
 });
 
-app.use(ensureAssertView);
 
 app.use(express.static(__dirname + '/public'));
+
+app.use('/secure', ensureSecureView, (req, res) => {
+    req.sendFile('./public/secure.html');
+});
+
+app.use(ensureAssetView);
+app.get('/asset', (req, res) => {
+    res.render('asset', { });
+});
+
+app.post('/asset', ensureAssetEdit, function (req, res) {
+    req.session.user.permissions = [req.body.permissions];
+    res.render('login', { });
+});
+
+
 
 // Start Server
 let server = http.createServer(app);
