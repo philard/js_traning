1d608f52aa6a9a5922dca16f161f8f6cf6966c94
Philip Ardley <philard@gmail.com>
2016-01-21 09:01:12 +0800

    Building user store and login.

--- express-authorization.js
+++ express-authorization.js
@@ -5,8 +5,8 @@ let http = require('http');
 let cookieParser = require('cookie-parser');
 let session = require('express-session');
 let bodyParser = require('body-parser');
-let authorization = require('express-authorization');
-
+let secure = require('./secure');
+let userStore = require('./user-store');
 let app = express();
 
 // all environments
@@ -20,51 +20,30 @@ app.use(bodyParser.json());
 app.use(cookieParser('your secret here'));
 app.use(session());
 
-// setup permission middleware
-authorization.ensureRequest.options = {
-    onDenied:function (req, res) {
-        req.session.urlPriorLogin = req.url;//Similar to Java http://stackoverflow.com/questions/14573654/spring-security-redirect-to-previous-page-after-succesful-login#answer-23796392
-        res.redirect('/login');
-    }
-    //redirectTo:'/login'
-};
 
-var ensureAssetView = authorization.ensureRequest.isPermitted('asset:view');
-var ensureAssetEdit = authorization.ensureRequest.isPermitted('asset:edit');
-var ensureSecureView = authorization.ensureSecureView.isPermitted('secure:view');
 
 app.get('/login', function (req, res) {
     res.render('login', { });
 });
 
-
 app.post('/login', function (req, res) {
-    req.session.user = {
-        username: req.body.username,
-        permissions: [ 'asset:view,edit', 'secure:' ]
-    };
-    res.redirect(req.session.urlPriorLogin);
-    delete req.session.urlPriorLogin;
+
+    let user = userStore[req.body.username];
+    if(user && user.password == req.body.password) {
+
+        req.session.user = user;
+        res.redirect(req.session.urlPriorLogin);
+        delete req.session.urlPriorLogin;
+
+    } else {
+        res.redirect('/login');
+    }
 });
 
 
 app.use(express.static(__dirname + '/public'));
 
-app.use('/secure', ensureSecureView, (req, res) => {
-    req.sendFile('./public/secure.html');
-});
-
-app.use(ensureAssetView);
-app.get('/asset', (req, res) => {
-    res.render('asset', { });
-});
-
-app.post('/asset', ensureAssetEdit, function (req, res) {
-    req.session.user.permissions = [req.body.permissions];
-    res.render('login', { });
-});
-
-
+app.use('/secure', secure);
 
 // Start Server
 let server = http.createServer(app);
