cbdea1ef5d6cd5642c684c1e06c6f070b7a08816
Philip_John_Ardley <philip_john_ardley@epam.com>
2016-01-25 16:27:50 +0800

    OMG!!!!!! Behold... The same mocha test (see Cow.js) are running BOTH in the browser (non amd/script tag loaded) AND in the npm test (ES6 babelified module loaded).
    For now on all my tests with conform to this style.

--- week2/app/__tests__/cow.spec.js
+++ week2/app/__tests__/cow.spec.js
@@ -1,28 +1,36 @@
+
+
+
+
+var Cow = Cow || require('../Cow.js').default;
+var chai = chai || require('chai');
 var expect = chai.expect;
 
-describe("Cow", function() {
-  describe("constructor", function() {
-    it("should have a default name", function() {
-      var cow = new Cow();
-      expect(cow.name).to.equal("Anon cow");
+
+  describe("Cow", function() {
+    describe("constructor", function() {
+      it("should have a default name", function() {
+        var cow = new Cow();
+        expect(cow.name).to.equal("Anon cow");
+      });
+
+      it("should set cow's name if provided", function() {
+        var cow = new Cow("Kate");
+        expect(cow.name).to.equal("Kate");
+      });
     });
 
-    it("should set cow's name if provided", function() {
-      var cow = new Cow("Kate");
-      expect(cow.name).to.equal("Kate");
+    describe("#greets", function() {
+      it("should throw if no target is passed in", function() {
+        expect(function() {
+          (new Cow()).greets();
+        }).to.throw(Error);
+      });
+
+      it("should greet passed target", function() {
+        var greetings = (new Cow("Kate")).greets("Baby");
+        expect(greetings).to.equal("Kate greets Baby");
+      });
     });
   });
 
-  describe("#greets", function() {
-    it("should throw if no target is passed in", function() {
-      expect(function() {
-        (new Cow()).greets();
-      }).to.throw(Error);
-    });
-
-    it("should greet passed target", function() {
-      var greetings = (new Cow("Kate")).greets("Baby");
-      expect(greetings).to.equal("Kate greets Baby");
-    });
-  });
-});
